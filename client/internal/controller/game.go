b4a4dc6842bdb30156d75c3b7ec158695c6e1a58
KatyNester <nesterenkokm@bk.ru>
2024-12-25 01:08:51 +0300

    Рефакторинг

--- client/internal/controller/game.go
+++ client/internal/controller/game.go
@@ -10,74 +10,80 @@ import (
 )
 
 func StartGame(ch chan struct{}, conn net.Conn) {
-	var req Proto.Request
-	var resp Proto.Response
 	// Логинимся
-	var username string
-	// Отправка имени
-	for {
-		fmt.Println("Please enter your username:")
-		fmt.Scanln(&username)
-		// Создаем команду
-		req = Proto.Request{Command: viper.GetString("sign"), Username: username}
-		// Отправляем серверу json
-		encoder := json.NewEncoder(conn)
-		err := encoder.Encode(req)
-		if err != nil {
-			log.Print(err.Error())
-			continue
-		}
-		// Чтение данных с вервера
-		decoder := json.NewDecoder(conn)
-		err = decoder.Decode(&resp)
-		// Ошибка при декодировании
-		if err != nil {
-			log.Print(err.Error())
-			continue
-		}
-		if resp.Cod != 200 {
-			fmt.Println(resp, "Попробуйте ещё раз")
-		}
-		// Всё норм, имя прошло
-		break
-	}
-
-	// Выбираем стратегию
-	for {
-	var choose int
-Loop:
-	for {
-		fmt.Println("Выберите: \n 1. Сетевая игра \n Одиночная игра")
-		fmt.Scanln(&choose)
-		switch choose {
-		case 1:
-			req = Proto.Request{Command: viper.GetString("game_user"), Username: username}
-			break Loop
-		case 2:
-			req = Proto.Request{Command: viper.GetString("game_server"), Username: username}
-			break Loop
-		default:
-			fmt.Println("Выбор не распознан.Нужно выбрать пункт 1 или 2.")
-		}
-	}
+	username := ChooseUsername(conn)
+	// Выбираем имя
+	ChooseStrategy(conn, username)
+}
+func Send(conn net.Conn, req Proto.Request) error {
 	// Отправляем серверу json
 	encoder := json.NewEncoder(conn)
 	err := encoder.Encode(req)
 	if err != nil {
 		log.Print(err.Error())
-
 	}
+	return err
+}
+func Accept(conn net.Conn) error {
+	var resp Proto.Response
 	// Чтение данных с вервера
 	decoder := json.NewDecoder(conn)
-	err = decoder.Decode(&resp)
+	err := decoder.Decode(&resp)
 	// Ошибка при декодировании
-	if err != nil {
-		log.Print(err.Error())
-		continue
+	if err != nil || resp.Cod != 200 {
+		log.Print(err.Error(), resp)
+		return err
 	}
-	if resp.Cod != 200 {
-		fmt.Println(resp, "Попробуйте ещё раз")
+	return nil
+}
+func ChooseUsername(conn net.Conn) string {
+	var username string
+	var err error
+	for {
+		fmt.Println("Please enter your username:")
+		fmt.Scanln(&username)
+		err = Send(conn, Proto.Request{Command: viper.GetString("sign"), Username: username})
+		if err != nil {
+			continue
+		}
+		err = Accept(conn)
+		if err != nil {
+			continue
+		}
+		// Всё норм, имя прошло
+		return username
 	}
-	// Всё норм, дейсвие прошло
-	break
+}
+func ChooseStrategy(conn net.Conn, username string) {
+	var err error
+	// Выбираем стратегию
+	var choose int
+	for {
+		fmt.Println("Выберите: \n 1. Сетевая игра \n Одиночная игра")
+		fmt.Scanln(&choose)
+		switch choose {
+		case 1:
+			err = Send(conn, Proto.Request{Command: viper.GetString("game_user"), Username: username})
+			if err != nil {
+				break
+			}
+			err = Accept(conn)
+			if err != nil {
+				break
+			}
+			return
+		case 2:
+			err = Send(conn, Proto.Request{Command: viper.GetString("game_server"), Username: username})
+			if err != nil {
+				break
+			}
+			err = Accept(conn)
+			if err != nil {
+				break
+			}
+			return
+		default:
+			fmt.Println("Выбор не распознан.Нужно выбрать пункт 1 или 2.")
+		}
+	}
 }
