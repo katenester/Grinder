1d1a2f52ca6b9d68a2e648088cd4db2083329444
KatyNester <nesterenkokm@bk.ru>
2024-12-25 00:39:45 +0300

    Сделана фича выбор стратегии (сетевая или одиночная)

--- client/internal/controller/game.go
+++ client/internal/controller/game.go
@@ -10,6 +10,8 @@ import (
 )
 
 func StartGame(ch chan struct{}, conn net.Conn) {
+	var req Proto.Request
+	var resp Proto.Response
 	// Логинимся
 	var username string
 	// Отправка имени
@@ -17,25 +19,65 @@ func StartGame(ch chan struct{}, conn net.Conn) {
 		fmt.Println("Please enter your username:")
 		fmt.Scanln(&username)
 		// Создаем команду
-		resp := Proto.Request{Command: viper.GetString("sign"), Username: username}
+		req = Proto.Request{Command: viper.GetString("sign"), Username: username}
 		// Отправляем серверу json
 		encoder := json.NewEncoder(conn)
-		err := encoder.Encode(resp)
+		err := encoder.Encode(req)
 		if err != nil {
 			log.Print(err.Error())
 			continue
 		}
 		// Чтение данных с вервера
-		var msg Proto.Response
 		decoder := json.NewDecoder(conn)
-		err = decoder.Decode(&msg)
+		err = decoder.Decode(&resp)
 		// Ошибка при декодировании
 		if err != nil {
 			log.Print(err.Error())
 			continue
 		}
-		if msg.Cod != 200 {
-			fmt.Println(msg, "Попробуйте ещё раз")
+		if resp.Cod != 200 {
+			fmt.Println(resp, "Попробуйте ещё раз")
+		}
+		// Всё норм, имя прошло
+		break
+	}
+
+	// Выбираем стратегию
+	for {
+	var choose int
+Loop:
+	for {
+		fmt.Println("Выберите: \n 1. Сетевая игра \n Одиночная игра")
+		fmt.Scanln(&choose)
+		switch choose {
+		case 1:
+			req = Proto.Request{Command: viper.GetString("game_user"), Username: username}
+			break Loop
+		case 2:
+			req = Proto.Request{Command: viper.GetString("game_server"), Username: username}
+			break Loop
+		default:
+			fmt.Println("Выбор не распознан.Нужно выбрать пункт 1 или 2.")
 		}
 	}
+	// Отправляем серверу json
+	encoder := json.NewEncoder(conn)
+	err := encoder.Encode(req)
+	if err != nil {
+		log.Print(err.Error())
+
+	}
+	// Чтение данных с вервера
+	decoder := json.NewDecoder(conn)
+	err = decoder.Decode(&resp)
+	// Ошибка при декодировании
+	if err != nil {
+		log.Print(err.Error())
+		continue
+	}
+	if resp.Cod != 200 {
+		fmt.Println(resp, "Попробуйте ещё раз")
+	}
+	// Всё норм, дейсвие прошло
+	break
 }
